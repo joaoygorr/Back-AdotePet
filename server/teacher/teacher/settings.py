ebdae5582a9caba9ccea3c3e0dbdf878c0ce24fa
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2022-05-19 16:59:45 -0400

    integradando com o mysql

--- server/teacher/teacher/settings.py
+++ server/teacher/teacher/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'teacher.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Teachers',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
