03b081d0cc4b7224fcdc01a272855652fc6ba258
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2022-05-20 16:01:13 -0400

    usando pycep-correios

--- server/teacher/web/services/zip_code_service.py
+++ server/teacher/web/services/zip_code_service.py
@@ -1,7 +1,5 @@
-import requests
+import pycep_correios
 
 def search_city_zip_Code(zip_code): 
-    response = requests.get(
-        f"http://viacep.com.br/ws/{zip_code}/json"
-    )
+    response = pycep_correios.get_address_from_cep(zip_code)
     return response
