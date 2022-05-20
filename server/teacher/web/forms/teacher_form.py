8e4355f00fdc34045d89db0073f47cc24e43e0ab
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2022-05-20 14:58:46 -0400

    retirando campo ibge

--- server/teacher/web/forms/teacher_form.py
+++ server/teacher/web/forms/teacher_form.py
@@ -1,26 +1,44 @@
 from django import forms
+import json
 # table Teacher
 from ..models import Teacher
+from ..services import zip_code_service
 
 class TeacherForm(forms.ModelForm): 
     cpf = forms.CharField(widget=forms.TextInput(attrs={"data-mask": "000.000.000-00"}))
     zip_code = forms.CharField(widget=forms.TextInput(attrs={"data-mask": "00000-000"}))
     phone = forms.CharField(widget=forms.TextInput(attrs={"data-mask": "(00) 00000-0000"}))
+    
     class Meta: 
         model = Teacher
-        fields = "__all__"
+        exclude = ("cod_ibge", )
     
     # Limpando cpf
     def clean_cpf(self): 
         cpf = self.cleaned_data["cpf"]
-        return cpf.replace(".", "").replace("-","")
+        return cpf.replace(".", "").replace("-", "")
     
     # Limpando ZipCode
     def clean_zip_code(self): 
         zip_code = self.cleaned_data["zip_code"]
-        return zip_code.replace("-","")
+        zip_code_formated = zip_code.replace("-","")
+        response = zip_code_service.search_city_zip_Code(zip_code_formated)
+        if response.status_code == 400:
+            raise forms.ValidationError("The zip code entered is incorrect! ")
+        city_api = json.loads(response.content)
+        if "erro" in city_api: 
+            raise forms.ValidationError("The specified zip code was not found.")
+        return zip_code_formated
     
     # Limpando phone
     def clean_phone(self): 
         phone = self.cleaned_data["phone"]
         return phone.replace("(", "").replace(")","").replace(" ","").replace("-", "")
+    
+    def save(self, commit=True): 
+        instance = super(TeacherForm, self).save(commit=False)
+        response = zip_code_service.search_city_zip_Code(self.cleaned_data.get('zip_code'))
+        city__api = json.loads(response.content)
+        instance.cod_ibge = city__api["cod_ibge"]
+        instance.save()
+        return instance
---
+++ server/teacher/web/services/zip_code_service.py
@@ -0,0 +1,8 @@
+from urllib import request
+import requests
+
+def search_city_zip_Code(zip_code): 
+    response = requests.get(
+        f"http://viacep.com.br/ws/{zip_code}/json"
+    )
+    return response
