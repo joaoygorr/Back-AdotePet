1c498ceb1a1c36b65641b61fd846f7a4328c716d
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2022-05-20 21:23:26 -0400

    add cors

--- server/teacher/teacher/settings.py
+++ server/teacher/teacher/settings.py
@@ -23,9 +23,12 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'easy_mask',
+    'corsheaders',
     'web',
     'api'
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
@@ -33,6 +36,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
