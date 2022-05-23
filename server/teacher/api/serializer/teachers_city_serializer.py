c30f41cec6501ffd6a0d2fcca04e899a977b258c
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2022-05-23 17:43:01 -0400

    enviando "district"

--- server/teacher/api/serializer/teachers_city_serializer.py
+++ server/teacher/api/serializer/teachers_city_serializer.py
@@ -4,4 +4,4 @@ from web.models import Teacher
 class TeacherCitySerializer(serializers.ModelSerializer): 
     class Meta:
         model = Teacher
-        fields = ('full_name', 'city', 'photo_user')
+        fields = ('full_name', 'city', 'district', 'photo_user')
